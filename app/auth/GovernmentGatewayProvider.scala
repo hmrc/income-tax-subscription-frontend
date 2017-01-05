5d1d8c4167a09199be6a4ea8cd25be433d818b6a
jas.johal <jas.johal@digital.hmrc.gov.uk>
2017-01-05 10:26:32 +0000

    Header year change

--- app/auth/GovernmentGatewayProvider.scala
+++ app/auth/GovernmentGatewayProvider.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 HM Revenue & Customs
+ * Copyright 2017 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,4 +31,3 @@ object GovernmentGatewayProvider {
   def handleSessionTimeout(implicit request: Request[_]): Future[Result] =
     Future.successful(Redirect(controllers.routes.SessionTimeoutController.timeout().url))
 }
-
