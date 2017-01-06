5d7a31ea2d412ffe0f1903d7d3dcec5cd6308b5e
jas.johal <jas.johal@digital.hmrc.gov.uk>
2017-01-06 09:41:58 +0000

    Added controller for Business Income Type and relevant routes

--- app/controllers/BusinessIncomeTypeController.scala
+++ app/controllers/BusinessIncomeTypeController.scala
@@ -1,8 +1,43 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
-/**
-  * Created by js_johal on 06/01/17.
-  */
-object BusinessIncomeTypeController {
+import auth.AuthorisedForIncomeTaxSA
+import config.{FrontendAppConfig, FrontendAuthConnector}
+import uk.gov.hmrc.play.frontend.controller.FrontendController
+import play.api.i18n.Messages.Implicits._
+import play.api.Play.current
+import uk.gov.hmrc.play.http.HttpResponse
 
+import scala.concurrent.Future
+
+object BusinessIncomeTypeController extends BusinessIncomeTypeController {
+  override lazy val applicationConfig = FrontendAppConfig
+  override lazy val authConnector = FrontendAuthConnector
+  override lazy val postSignInRedirectUrl = FrontendAppConfig.ggSignInContinueUrl
+}
+
+trait BusinessIncomeTypeController extends FrontendController with AuthorisedForIncomeTaxSA {
+
+  val showBusinessIncomeType = Authorised.async { implicit user => implicit request =>
+    Future.successful(NotImplemented)
+  }
+
+  val submitBusinessIncomeType = Authorised.async { implicit user => implicit request =>
+    Future.successful(NotImplemented)
+  }
 }
