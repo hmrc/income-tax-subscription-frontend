2283ebfb12f23f250ce37e048dc1f45fbd0e61a1
jas.johal <jas.johal@digital.hmrc.gov.uk>
2017-01-06 09:00:52 +0000

    Added controller for Terms and relevant routes

---
+++ app/controllers/TermsController.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import auth.AuthorisedForIncomeTaxSA
+import config.{FrontendAppConfig, FrontendAuthConnector}
+import uk.gov.hmrc.play.frontend.controller.FrontendController
+import play.api.i18n.Messages.Implicits._
+import play.api.Play.current
+import uk.gov.hmrc.play.http.HttpResponse
+
+import scala.concurrent.Future
+
+object TermsController {
+  override lazy val applicationConfig = FrontendAppConfig
+  override lazy val authConnector = FrontendAuthConnector
+  override lazy val postSignInRedirectUrl = FrontendAppConfig.ggSignInContinueUrl
+}
+
+trait TermsController extends FrontendController with AuthorisedForIncomeTaxSA {
+  val showTerms = Authorised.async { implicit user => implicit request =>
+    Future.successful(NotImplemented)
+  }
+
+  val submitTerms = Authorised.async { implicit user => implicit request =>
+    Future.successful(NotImplemented)
+  }
+}
