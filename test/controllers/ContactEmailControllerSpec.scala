22e91cd6e25d86a7e05ee295519652a7079b9f23
jas.johal <jas.johal@digital.hmrc.gov.uk>
2017-01-06 11:00:16 +0000

    Added test for the ContactEmailController

---
+++ test/controllers/ContactEmailControllerSpec.scala
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import akka.actor._
+import akka.stream._
+import assets.MessageLookup
+import auth._
+import config.{FrontendAppConfig, FrontendAuthConnector}
+import org.jsoup.Jsoup
+import org.scalatestplus.play.{OneAppPerTest, PlaySpec}
+import play.api.http.Status
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+
+class ContactEmailControllerSpec extends PlaySpec with OneAppPerTest {
+
+  object TestContactEmailController extends ContactEmailController {
+    override lazy val applicationConfig = MockConfig
+    override lazy val authConnector = MockAuthConnector
+    override lazy val postSignInRedirectUrl = MockConfig.ggSignInContinueUrl
+  }
+  implicit val system = ActorSystem()
+  implicit val materializer = ActorMaterializer()
+
+  "The ContactEmailController controller" should {
+    "use the correct applicationConfig" in {
+      ContactEmailController.applicationConfig must be (FrontendAppConfig)
+    }
+    "use the correct authConnector" in {
+      ContactEmailController.authConnector must be (FrontendAuthConnector)
+    }
+    "use the correct postSignInRedirectUrl" in {
+      ContactEmailController.postSignInRedirectUrl must be (FrontendAppConfig.ggSignInContinueUrl)
+    }
+  }
+
+  "Calling the showContactEmail action of the ContactEmailController with an authorised user" should {
+
+    lazy val result = TestContactEmailController.showContactEmail(authenticatedFakeRequest())
+
+    "return unimplemented (501)" in {
+      status(result) must be (Status.NOT_IMPLEMENTED)
+    }
+  }
+
+  "Calling the showContactEmail action of the ContactEmailController with an unauthorised user" should {
+
+    lazy val result = TestContactEmailController.showContactEmail(FakeRequest())
+
+    "return 303" in {
+      status(result) must be (Status.SEE_OTHER)
+    }
+  }
+
+  "Calling the submitContactEmail action of the ContactEmailController with an authorised user" should {
+
+    lazy val result = TestContactEmailController.submitContactEmail(authenticatedFakeRequest())
+
+    "return unimplemented (501)" in {
+      status(result) must be (Status.NOT_IMPLEMENTED)
+    }
+  }
+
+  "Calling the submitContactEmail action of the ContactEmailController with an unauthorised user" should {
+
+    lazy val result = TestContactEmailController.submitContactEmail(FakeRequest())
+
+    "return 303" in {
+      status(result) must be (Status.SEE_OTHER)
+    }
+  }
+}
