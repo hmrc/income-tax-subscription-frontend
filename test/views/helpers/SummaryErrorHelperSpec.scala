daae0218726bef23de60ad528577871144e8d9ab
sudhakar <sudha.kayyala@digital.hmrc.gov.uk>
2017-01-11 17:01:17 +0000

    summaryerror unit test

--- test/views/helpers/SummaryErrorHelperSpec.scala
+++ test/views/helpers/SummaryErrorHelperSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package views.helpers
 
 import forms.validation.ConstraintUtil._
@@ -15,7 +31,7 @@ import play.twirl.api.Html
 
 class SummaryErrorHelperSpec extends PlaySpec with OneServerPerSuite {
 
-  private def checkboxHelper(form: Form[_])
+  private def summaryErrorHelper(form: Form[_])
   = views.html.helpers.summaryErrorHelper(form)(applicationMessages)
 
   implicit class HtmlFormatUtil(html: Html) {
@@ -46,12 +62,19 @@ class SummaryErrorHelperSpec extends PlaySpec with OneServerPerSuite {
     )(TestData.apply)(TestData.unapply)
   )
 
-  "checkboxHelper" should {
-    "no error" in {
+  "SummeryErrorHelper" should {
+    "if the form not  present error it should continue" in {
+      val page = summaryErrorHelper(testForm)
+      val doc = page.doc
 
     }
-    "errors present" in {
 
+    "SummeryErrorHelper" should {
+      "if the form present error it should display error message" in {
+        val page = summaryErrorHelper(testForm.fill(TestData("data", " ", "data")))
+        val doc = page.doc
+
+      }
     }
   }
 }
