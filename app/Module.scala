9c8f5846a466148559dd857c7cd2908838effb49
Su-Yang Yu <su-yang.yu@digital.hmrc.gov.uk>
2017-01-25 15:39:49 +0000

    changed routesGenerator := InjectedRoutesGenerator

--- app/Module.scala
+++ app/Module.scala
@@ -17,6 +17,7 @@
 import com.google.inject.AbstractModule
 import config.{AppConfig, FrontendAppConfig}
 import uk.gov.hmrc.http.cache.client.SessionCache
+import uk.gov.hmrc.play.http.{HttpDelete, HttpGet, HttpPost, HttpPut}
 import uk.gov.hmrc.play.http.ws._
 
 class Module extends AbstractModule {
@@ -24,9 +25,13 @@ class Module extends AbstractModule {
   override def configure(): Unit = {
     bind(classOf[SessionCache]).to(classOf[config.SessionCache]).asEagerSingleton()
     bind(classOf[WSGet]).to(classOf[config.WSHttp]).asEagerSingleton()
+    bind(classOf[HttpGet]).to(classOf[config.WSHttp]).asEagerSingleton()
     bind(classOf[WSPost]).to(classOf[config.WSHttp]).asEagerSingleton()
+    bind(classOf[HttpPost]).to(classOf[config.WSHttp]).asEagerSingleton()
     bind(classOf[WSDelete]).to(classOf[config.WSHttp]).asEagerSingleton()
+    bind(classOf[HttpDelete]).to(classOf[config.WSHttp]).asEagerSingleton()
     bind(classOf[WSPut]).to(classOf[config.WSHttp]).asEagerSingleton()
+    bind(classOf[HttpPut]).to(classOf[config.WSHttp]).asEagerSingleton()
     bind(classOf[WSHttp]).to(classOf[config.WSHttp]).asEagerSingleton()
     bind(classOf[AppConfig]).to(classOf[FrontendAppConfig]).asEagerSingleton()
   }
