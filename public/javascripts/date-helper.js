0b9a2a3d396b37cf2b4f034ca8ff77219414eb0f
Su-Yang Yu <su-yang.yu@digital.hmrc.gov.uk>
2017-03-15 11:22:43 +0000

    updated date-helper behaviour, also disabled inputs from none numeric characters

--- public/javascripts/date-helper.js
+++ public/javascripts/date-helper.js
@@ -4,36 +4,43 @@ $(document).ready(function () {
         $year = $('input[name$="dateYear"]');
     var BACKSPACE = 8;
     var ZERO = 48;
+    var ZERO_NUMPAD = 96;
     var NINE = 57;
+    var NINE_NUMPAD = 105;
 
-    function checkDay(key, elem) {
+    function dayKeyUp(key, elem) {
         var name = elem.name;
         var newValue = $(elem).val();
         var month = name.replace("dateDay", "dateMonth");
 
-        if (newValue.length == 2) {
+        if (key != BACKSPACE && newValue.length == 2) {
             $(document.getElementById(month)).focus();
         }
     }
 
-    function checkMonth(key, elem) {
+    function monthKeyDown(key, elem) {
         var name = elem.name;
         var newValue = $(elem).val();
         var day = name.replace("dateMonth", "dateDay");
         var year = name.replace("dateMonth", "dateYear");
 
-        console.log(key)
         if (key == BACKSPACE && newValue.length == 0) {
             $(document.getElementById(day)).focus();
         }
-
-        if (newValue.length == 2) {
-            $(document.getElementById(year)).focus();
-        }
-
     }
 
-    function checkYear(key, elem) {
+    function monthKeyUp(key, elem) {
+        var name = elem.name;
+        var newValue = $(elem).val();
+        var day = name.replace("dateMonth", "dateDay");
+        var year = name.replace("dateMonth", "dateYear");
+
+        if (key != BACKSPACE && newValue.length == 2) {
+            $(document.getElementById(year)).focus();
+        }
+    }
+
+    function yearKeyDown(key, elem) {
         var name = elem.name;
         var newValue = $(elem).val();
         var month = name.replace("dateYear", "dateMonth");
@@ -41,31 +48,56 @@ $(document).ready(function () {
         if (key == BACKSPACE && newValue.length == 0) {
             $(document.getElementById(month)).focus();
         }
-
     }
 
-    function trigger(key) {
-        return (key >= ZERO && key <= NINE ) || key == BACKSPACE
+    function inputFilter(event) {
+        var key = event.which;
+        if ((!modifier && key >= ZERO && key <= NINE) ||
+            (!modifier && key >= ZERO_NUMPAD && key <= NINE_NUMPAD) ||
+            key == BACKSPACE) {
+            return true
+        }
+        event.preventDefault();
+        return false
     }
+
+    $day.on('keydown', function (e) {
+        inputFilter(e);
+    });
 
     $day.on('keyup', function (e) {
         var key = e.which;
-        if (trigger(key)) {
-            checkDay(key, this);
+        dayKeyUp(key, this);
+    });
+
+    $month.on('keydown', function (e) {
+        if (inputFilter(e)) {
+            var key = e.which;
+            monthKeyDown(key, this);
         }
     });
 
     $month.on('keyup', function (e) {
         var key = e.which;
-        if (trigger(key)) {
-            checkMonth(key, this);
+        monthKeyUp(key, this);
+    });
+
+    $year.on('keydown', function (e) {
+        if (inputFilter(e)) {
+            var key = e.which;
+            yearKeyDown(key, this);
         }
     });
 
-    $year.on('keyup', function (e) {
-        var key = e.which;
-        if (trigger(key)) {
-            checkYear(key, this);
+    var modifier = false;
+    window.onkeydown = function (e) {
+        if (e.which >= 16 && e.which <= 19) {
+            modifier = true;
         }
-    });
+    };
+    window.onkeyup = function (e) {
+        if (e.which >= 16 && e.which <= 19) {
+            modifier = false;
+        }
+    };
 });
