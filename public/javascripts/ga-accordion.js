318b3ac70cd60307a1c733f7e505587bab4177de
robert-hawkes <robert.hawkes@digital.hmrc.gov.uk>
2017-05-05 13:57:01 +0100

    SAR-580 - Removed console log

--- public/javascripts/ga-accordion.js
+++ public/javascripts/ga-accordion.js
@@ -3,7 +3,6 @@ var open = false;
 function markAccordionOpen(label) {
     if (!open) {
         open = true;
-        console.log("Logged accordion open: " + label);
         ga('send', 'event', 'itsa', 'opened accordion', label);
     }
 }
