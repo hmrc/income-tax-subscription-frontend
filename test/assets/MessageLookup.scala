5ce43aba048d9ed43730e866914a01e8ed4a5bba
Su-Yang Yu <su-yang.yu@digital.hmrc.gov.uk>
2017-05-10 12:33:10 +0100

    SAR-615: updated the error message for terms of participation as per ticket

--- test/assets/MessageLookup.scala
+++ test/assets/MessageLookup.scala
@@ -300,7 +300,7 @@ object MessageLookup {
     }
 
     object Terms {
-      val empty = "You must confirm T&C's to continue"
+      val empty = "You must accept the terms of participation to continue"
     }
 
     object ExitSurvey {
