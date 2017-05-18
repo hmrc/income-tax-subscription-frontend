544b329e8a0d4521e00f3359a81380dbe63cac6c
jas.johal <jas.johal@digital.hmrc.gov.uk>
2017-05-18 22:01:01 +0100

    Made change to event

--- public/javascripts/ga-bta.js
+++ public/javascripts/ga-bta.js
@@ -3,7 +3,7 @@ $(document).ready($(function () {
     var $btaLink = $("#bta");
 
     $btaLink.on('click', function (e) {
-            ga('send', 'event', 'link clicked', 'bta link');
+            ga('send', 'event', 'itsa', 'bta link clicked');
     });
 
 }));
