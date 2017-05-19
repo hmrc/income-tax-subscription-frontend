ab7e47547261b2355d811a8af346f173e74a0377
Su-Yang Yu <su-yang.yu@digital.hmrc.gov.uk>
2017-05-19 09:30:16 +0100

    updated ga feedback to ensure ga event is sent before the link is triggered

--- public/javascripts/ga-feedback.js
+++ public/javascripts/ga-feedback.js
@@ -3,8 +3,17 @@ $(document).ready($(function () {
     var $feedbackLink = $("#beta-banner-feedback");
 
     $feedbackLink.on('click', function (e) {
+        e.preventDefault();
         var pageTitle = $('title').text();
-        ga('send', 'event', 'itsa', 'feedback link clicked', pageTitle);
+        if (typeof ga === "function" && $feedbackLink != undefined) {
+            ga('send', 'event', 'itsa', 'feedback link clicked', pageTitle, {
+                hitCallback: function () {
+                    window.location = $feedbackLink.attr("href")
+                }
+            });
+        }else {
+            window.location = $feedbackLink.attr("href")
+        }
     });
 
 }));
