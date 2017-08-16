e46c7743def70f1d3afceada89c3fee0572d226e
robert-hawkes <robert.hawkes@digital.hmrc.gov.uk>
2017-08-16 15:36:45 +0100

    SAR-679 - Refactored subscription flow to isolate unit tests

--- test/connectors/SubscriptionConnectorSpec.scala
+++ test/connectors/SubscriptionConnectorSpec.scala
@@ -17,10 +17,8 @@
 package connectors
 
 import connectors.mocks.TestSubscriptionConnector
-import connectors.models.subscription.SubscriptionResponse.{BadlyFormattedSubscriptionResponse, SubscriptionFailureResponse, SubscriptionResponse, SubscriptionSuccess}
 import org.scalatest.Matchers._
 import org.scalatest.{EitherValues, OptionValues}
-import play.api.test.Helpers._
 import utils.TestConstants._
 
 class SubscriptionConnectorSpec extends TestSubscriptionConnector with EitherValues with OptionValues {
@@ -29,24 +27,6 @@ class SubscriptionConnectorSpec extends TestSubscriptionConnector with EitherVal
     "Post to the correct url" in {
       TestSubscriptionConnector.subscriptionUrl(testNino) should endWith(s"/income-tax-subscription/subscription/$testNino")
     }
-
-//    def call: SubscriptionResponse = await(TestSubscriptionConnector.subscribe(request = testSubmissionRequest))
-//
-//    "return the success response as an object" in {
-//      setupMockSubscribeSuccess(testSubmissionRequest)
-//
-//      call.right.value shouldBe SubscriptionSuccess(testMTDID)
-//    }
-//
-//    "return an error if the response is badly formatted" in {
-//      setupMockSubscribeEmptyBody(testSubmissionRequest)
-//      call.left.value shouldBe BadlyFormattedSubscriptionResponse
-//    }
-//
-//    "return the correct error if the middle service indicated a bad request" in {
-//      setupMockSubscribeBadRequest(testSubmissionRequest)
-//      call.left.value shouldBe SubscriptionFailureResponse(BAD_REQUEST)
-//    }
   }
 
   "SubscriptionConnector.getSubscription" should {
@@ -54,23 +34,6 @@ class SubscriptionConnectorSpec extends TestSubscriptionConnector with EitherVal
     "GET to the correct url" in {
       TestSubscriptionConnector.subscriptionUrl(testNino) should endWith(s"/income-tax-subscription/subscription/$testNino")
     }
-
-//    def result: Either[SubscriptionFailureResponse, Option[SubscriptionSuccess]] = await(TestSubscriptionConnector.getSubscription(testNino))
-//
-//    "return the success response as an object" in {
-//      setupMockGetSubscriptionSuccess(testNino)
-//      result.right.value shouldBe Some(SubscriptionSuccess(testMTDID))
-//    }
-//
-//    "return an empty OK response as None" in {
-//      setupMockGetSubscriptionEmptyBody(testNino)
-//      result.right.value shouldBe empty
-//    }
-//
-//    "return fail if the middle service indicated a bad request" in {
-//      setupMockGetSubscriptionBadRequest(testNino)
-//      result.left.value shouldBe SubscriptionFailureResponse(BAD_REQUEST)
-//    }
   }
 
 }
