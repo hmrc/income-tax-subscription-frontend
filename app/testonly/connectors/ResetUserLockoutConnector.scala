f32e44b89e9061646772cbc44a1ca17791cc576e
robert-hawkes <robert.hawkes@digital.hmrc.gov.uk>
2017-09-07 14:20:15 +0100

    SAR-907 - Copy over user matching functionality

---
+++ app/testonly/connectors/ResetUserLockoutConnector.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package testonly.connectors
+
+import javax.inject.{Inject, Singleton}
+
+import config.AppConfig
+import uk.gov.hmrc.play.http.{HeaderCarrier, HttpGet, HttpResponse}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class ResetUserLockoutConnector @Inject()(val appConfig: AppConfig,
+                                          val httpGet: HttpGet
+                                          )(implicit ec: ExecutionContext) {
+
+  lazy val resetUrl = appConfig.clientMatchingUrl + ResetUserLockoutConnector.resetUri
+
+  def resetLockout(implicit hc: HeaderCarrier): Future[HttpResponse] = httpGet.GET(resetUrl)
+
+}
+
+object ResetUserLockoutConnector {
+  def resetUri: String = s"/test-only/reset-agent-lockout"
+}
+
---
+++ app/testonly/controllers/ResetUserLockoutController.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package testonly.controllers
+
+import javax.inject.{Inject, Singleton}
+
+import config.BaseControllerConfig
+import play.api.mvc.{Action, AnyContent}
+import testonly.connectors.ResetUserLockoutConnector
+import uk.gov.hmrc.play.frontend.controller.FrontendController
+
+@Singleton
+class ResetUserLockoutController @Inject()(val baseConfig: BaseControllerConfig,
+                                           val resetUserLockoutConnector: ResetUserLockoutConnector) extends FrontendController {
+
+  val resetLockout: Action[AnyContent] = Action.async { implicit request =>
+    for {
+      reset <- resetUserLockoutConnector.resetLockout
+    } yield reset.status match {
+      case OK => Ok("Agent lockout has been reset")
+      case status => InternalServerError(s"Unexpected failure returned status=$status body=${reset.body}")
+    }
+  }
+
+}
