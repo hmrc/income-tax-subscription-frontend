b9ae837bd58042a3101513ed0351612baece11d0
Su-Yang Yu <su-yang.yu@digital.hmrc.gov.uk>
2017-10-19 10:42:41 +0100

    SAR-1033: removed no thanks on ur-banner

--- public/javascripts/ur-banner.js
+++ public/javascripts/ur-banner.js
@@ -3,16 +3,5 @@
  * Includes: core.js, widget.js, mouse.js, position.js, accordion.js, autocomplete.js, button.js, datepicker.js, dialog.js, draggable.js, droppable.js, effect.js, effect-blind.js, effect-bounce.js, effect-clip.js, effect-drop.js, effect-explode.js, effect-fade.js, effect-fold.js, effect-highlight.js, effect-puff.js, effect-pulsate.js, effect-scale.js, effect-shake.js, effect-size.js, effect-slide.js, effect-transfer.js, menu.js, progressbar.js, resizable.js, selectable.js, selectmenu.js, slider.js, sortable.js, spinner.js, tabs.js, tooltip.js
  * Copyright 2015 jQuery Foundation and other contributors; Licensed MIT */
 $(document).ready($(function () {
-
-    var cookieData = GOVUK.getCookie("mtd_ur_panel");
-    if (cookieData == null) {
-        $("#ur-panel").addClass("banner-panel--show");
-    }
-
-    $(".banner-panel__close").on("click", function (e) {
-        e.preventDefault();
-        GOVUK.setCookie("mtd_ur_panel", 1, 99999999999);
-        $("#ur-panel").removeClass("banner-panel--show");
-    });
-
+    $("#ur-panel").addClass("banner-panel--show");
 }));
