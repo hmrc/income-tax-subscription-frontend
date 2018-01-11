e396697b46c50a5e0e3d6c7ab3bb6be0d810a12c
Su-Yang Yu <su-yang.yu@digital.hmrc.gov.uk>
2018-01-11 14:01:40 +0000

    SAR-1233: added service to send the subscription data to the authorised agent store

---
+++ app/incometax/unauthorisedagent/services/SubscriptionStorePersistenceService.scala
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package incometax.unauthorisedagent.services
+
+import javax.inject.{Inject, Singleton}
+
+import agent.forms.{AccountingPeriodPriorForm, OtherIncomeForm}
+import agent.models.AccountingPeriodPriorModel
+import agent.services.CacheUtil._
+import agent.services.KeystoreService
+import core.config.AppConfig
+import incometax.incomesource.models.OtherIncomeModel
+import incometax.subscription.models.IncomeSourceType
+import incometax.unauthorisedagent.connectors.SubscriptionStoreConnector
+import incometax.unauthorisedagent.httpparsers.StoreSubscriptionResponseHttpParser.StoreSubscriptionResponse
+import incometax.unauthorisedagent.models.StoredSubscription
+import uk.gov.hmrc.http.HeaderCarrier
+
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class SubscriptionStorePersistenceService @Inject()(subscriptionStoreConnector: SubscriptionStoreConnector,
+                                                    keystoreService: KeystoreService,
+                                                    appConfig: AppConfig)(implicit ec: ExecutionContext) {
+
+  def storeSubscription(arn: String, nino: String)(implicit hc: HeaderCarrier): Future[StoreSubscriptionResponse] =
+    getSubscriptionData(arn).flatMap(subscriptionData => subscriptionStoreConnector.storeSubscriptionData(nino, subscriptionData))
+
+  private[services] def getSubscriptionData(arn: String)(implicit hc: HeaderCarrier): Future[StoredSubscription] =
+    for {
+      optCache <- keystoreService.fetchAll()
+      cache = optCache.get
+      incomeSource = cache.getIncomeSource
+      otherIncome = cache.getOtherIncome()
+      accountingPeriodPrior = cache.getAccountingPeriodPrior()
+      accountingPeriodDates = cache.getAccountingPeriodDate()
+      businessName = cache.getBusinessName()
+      accountingMethod = cache.getAccountingMethod()
+    } yield StoredSubscription(
+      arn,
+      IncomeSourceType(incomeSource.get.source),
+      otherIncome.map {
+        case OtherIncomeModel(OtherIncomeForm.option_yes) => true
+        case OtherIncomeModel(OtherIncomeForm.option_no) => false
+      }.get,
+      accountingPeriodPrior.map {
+        case AccountingPeriodPriorModel(AccountingPeriodPriorForm.option_yes) => true
+        case AccountingPeriodPriorModel(AccountingPeriodPriorForm.option_no) => false
+      },
+      accountingPeriodDates.map(_.startDate),
+      accountingPeriodDates.map(_.endDate),
+      businessName.map(_.businessName),
+      accountingMethod.map(_.accountingMethod)
+    )
+
+
+}
