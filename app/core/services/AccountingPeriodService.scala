3b15ccd412612f977fe15b48394a2181cae0ecb1
marjan <marjan.mubarok@digital.hmrc.gov.uk>
2019-09-17 10:41:27 +0100

    SAR-4540 Added service for calculating whether accounting period is valid.

---
+++ app/core/services/AccountingPeriodService.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package core.services
+
+import java.time.LocalDate
+
+import incometax.util.{AccountingPeriodUtil, CurrentDateProvider}
+import javax.inject.{Inject, Singleton}
+
+@Singleton
+class AccountingPeriodService @Inject()(currentDateProvider: CurrentDateProvider) {
+  def accountingPeriodElligibilty(starDate: LocalDate, endDate: LocalDate): Boolean = {
+    isWholeYear(starDate, endDate) && isValidTaxYear(starDate, endDate)
+  }
+
+  def isWholeYear(startDate: LocalDate, endDate: LocalDate): Boolean = {
+    startDate.minusDays(1).plusYears(1) == endDate
+  }
+
+  def isValidTaxYear(startDate: LocalDate, endDate: LocalDate): Boolean = {
+    val currentTaxYearEndYear = AccountingPeriodUtil.getTaxEndYear(currentDateProvider.getCurrentDate())
+
+    endDate.getYear >= currentTaxYearEndYear && endDate.getYear < currentTaxYearEndYear + 1
+  }
+
+}
