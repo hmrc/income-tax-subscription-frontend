c043449782f8da43f01361d7902cf018c9778276
scully.thornton <scullythornton1@users.noreply.github.com>
2020-03-02 12:09:09 +0000

    MISUV-305:create check enrolment allocation service

---
+++ test/services/CheckEnrolmentAllocationServiceSpec.scala
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package services
+
+import java.util.UUID
+
+import connectors.agent.httpparsers.EnrolmentStoreProxyHttpParser
+import connectors.agent.httpparsers.EnrolmentStoreProxyHttpParser.EnrolmentStoreProxyFailure
+import connectors.agent.mocks.MockEnrolmentStoreProxyConnector
+import org.scalatest.{Matchers, WordSpec}
+import play.api.test.Helpers._
+import services.CheckEnrolmentAllocationService.UnexpectedEnrolmentStoreProxyFailure
+import uk.gov.hmrc.domain.Generator
+import uk.gov.hmrc.http.HeaderCarrier
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class CheckEnrolmentAllocationServiceSpec extends WordSpec with Matchers
+  with MockEnrolmentStoreProxyConnector {
+
+  object TestCheckEnrolmentAllocationService extends CheckEnrolmentAllocationService(
+    mockEnrolmentStoreProxyConnector
+  )
+
+  implicit val hc: HeaderCarrier = HeaderCarrier()
+
+  "getGroupIdForEnrolment" when {
+    "EnrolmentStoreProxy returns EnrolmentNotAllocated" should {
+      "returns EnrolmentNotAllocated" in {
+        lazy val testUtr: String = new Generator().nextAtedUtr.utr
+        mockGetAllocatedEnrolment(testUtr)(Future.successful(Right(EnrolmentStoreProxyHttpParser.EnrolmentNotAllocated)))
+
+        val res = TestCheckEnrolmentAllocationService.getGroupIdForEnrolment(testUtr)
+
+        await(res) shouldBe Right(CheckEnrolmentAllocationService.EnrolmentNotAllocated)
+      }
+    }
+    "EnrolmentStoreProxy returns EnrolmentAlreadyAllocated" should {
+      "return EnrolmentAlreadyAllocated" in {
+        lazy val testUtr: String = new Generator().nextAtedUtr.utr
+        val testGroupId: String = UUID.randomUUID.toString
+        mockGetAllocatedEnrolment(testUtr)(
+          Future.successful(Right(EnrolmentStoreProxyHttpParser.EnrolmentAlreadyAllocated(testGroupId)))
+        )
+
+        val res = TestCheckEnrolmentAllocationService.getGroupIdForEnrolment(testUtr)
+
+        await(res) shouldBe Left(CheckEnrolmentAllocationService.EnrolmentAlreadyAllocated(testGroupId))
+      }
+    }
+    "EnrolmentStoreProxy returns an unexpected failure" should {
+      "return Failure" in {
+        lazy val testUtr: String = new Generator().nextAtedUtr.utr
+        mockGetAllocatedEnrolment(testUtr)(Future.successful(Left(EnrolmentStoreProxyFailure(BAD_REQUEST))))
+
+        val res = TestCheckEnrolmentAllocationService.getGroupIdForEnrolment(testUtr)
+
+        await(res) shouldBe Left(UnexpectedEnrolmentStoreProxyFailure(BAD_REQUEST))
+      }
+    }
+  }
+}
+
+
