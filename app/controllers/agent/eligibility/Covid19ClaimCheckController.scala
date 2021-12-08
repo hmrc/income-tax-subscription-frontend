fb34b977704a1f5a04f5759add1ea89032bc8880
Samir Benzenine <samir.benzenine@digital.hmrc.gov.uk>
2021-12-08 11:03:59 +0000

    ITSASU-673
    Migrate Covid19 Claim Check page
    
    Signed-off-by: Samir Benzenine <samir.benzenine@digital.hmrc.gov.uk>

--- app/controllers/agent/eligibility/Covid19ClaimCheckController.scala
+++ app/controllers/agent/eligibility/Covid19ClaimCheckController.scala
@@ -25,13 +25,14 @@ import models.audits.EligibilityAnswerAuditing.EligibilityAnswerAuditModel
 import models.{No, Yes}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import services.{AuditingService, AuthService}
-import views.html.agent.eligibility.covid_19_claim_check
+import views.html.agent.eligibility.Covid19ClaimCheck
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.ExecutionContext
 
 @Singleton
-class Covid19ClaimCheckController @Inject()(val auditingService: AuditingService,
+class Covid19ClaimCheckController @Inject()(val covid19ClaimCheck: Covid19ClaimCheck,
+                                             val auditingService: AuditingService,
                                             val authService: AuthService)
                                            (implicit val ec: ExecutionContext,
                                             mcc: MessagesControllerComponents,
@@ -42,7 +43,7 @@ class Covid19ClaimCheckController @Inject()(val auditingService: AuditingService
       if (isEnabled(RemoveCovidPages)) {
         Redirect(routes.OtherSourcesOfIncomeController.show())
       } else {
-        Ok(covid_19_claim_check(covid19ClaimCheckForm, routes.Covid19ClaimCheckController.submit(), backUrl))
+        Ok(covid19ClaimCheck(covid19ClaimCheckForm, routes.Covid19ClaimCheckController.submit(), backUrl))
       }
   }
 
@@ -50,7 +51,7 @@ class Covid19ClaimCheckController @Inject()(val auditingService: AuditingService
     implicit user =>
       val arn: Option[String] = user.arn
       covid19ClaimCheckForm.bindFromRequest.fold(
-        formWithErrors => BadRequest(covid_19_claim_check(formWithErrors, routes.Covid19ClaimCheckController.submit(), backUrl)), {
+        formWithErrors => BadRequest(covid19ClaimCheck(formWithErrors, routes.Covid19ClaimCheckController.submit(), backUrl)), {
           case Yes =>
             auditingService.audit(EligibilityAnswerAuditModel(EligibilityAnswerAuditing.eligibilityAnswerAgent, eligible = false, "yes",
               "claimedCovidGrant", arn))
