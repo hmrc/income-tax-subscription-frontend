bca3b2bc7e2bfb397e4bbccfafe1c0a12886786d
Justin Rowles <edward.rowles@digital.hmrc.gov.uk>
2022-04-05 11:16:15 +0100

    Feature/itsasu 416 (#985)
    [ITSASU-416] Implement throttling pages, Improve script to handle sm switches such as --offline

---
+++ app/controllers/agent/ThrottlingController.scala
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.agent
+
+import auth.individual.BaseFrontendController
+import config.AppConfig
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import services.{AuditingService, AuthService}
+import views.html.agent.Throttle
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class ThrottlingController @Inject()(val auditingService: AuditingService,
+                                     val authService: AuthService,
+                                     throttle: Throttle)
+                                    (implicit mcc: MessagesControllerComponents,
+                                     val ec: ExecutionContext,
+                                     val appConfig: AppConfig) extends BaseFrontendController {
+
+  def show(): Action[AnyContent] = Action.async { implicit request => Future.successful(Ok(throttle(backLink()))) }
+
+  private def backLink() = Some(controllers.agent.routes.HomeController.index.url)
+
+}
