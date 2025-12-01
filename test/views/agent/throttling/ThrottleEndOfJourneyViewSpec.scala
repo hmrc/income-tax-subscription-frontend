85abda87f15610104354d289c6d99bf6ec865d43
NoorMhd7 <146771080+NoorMhd7@users.noreply.github.com>
2025-12-01 14:01:23 +0000

    Updated line 2 test case on the agent throttle end page

--- test/views/agent/throttling/ThrottleEndOfJourneyViewSpec.scala
+++ test/views/agent/throttling/ThrottleEndOfJourneyViewSpec.scala
@@ -51,7 +51,7 @@ class ThrottleEndOfJourneyViewSpec extends ViewSpec {
       document1.select("main").select("p").first.text() mustBe messages.line_1
     }
     "have a line_2" in {
-      document1.select("main").select("p").next().first().text() mustBe messages.line_2 + " " + LocalDate.now().plusDays(30).format(DateTimeFormatter.ofPattern("d MMMM yyyy"))
+      document1.select("main").select("p").next().first().text() mustBe messages.line_2 (LocalDate.now().plusDays(30).format(DateTimeFormatter.ofPattern("d MMMM yyyy")))
     }
     "have a continueButton" in {
       document1.select("main").select("button").first().text() mustBe messages.continueButton
