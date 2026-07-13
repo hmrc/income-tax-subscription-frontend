e73696ae889510c0e31cba3393348227af7ea4a8
AlexRimmerHMRC <31283208+AlexRimmerHMRC@users.noreply.github.com>
2026-07-13 16:09:47 +0100

    ITSASU-5236 - Update WhatYouNeedToDoController to use identifier action and sign up journey refiner

--- it/test/controllers/individual/WhatYouNeedToDoControllerISpec.scala
+++ it/test/controllers/individual/WhatYouNeedToDoControllerISpec.scala
@@ -16,32 +16,43 @@
 
 package controllers.individual
 
-import common.Constants.ITSASessionKeys
-import connectors.stubs.{IncomeTaxSubscriptionConnectorStub, SessionDataConnectorStub}
 import helpers.ComponentSpecBase
-import helpers.IntegrationTestConstants.IndividualURI
-import helpers.IntegrationTestModels.testAccountingYearCurrent
+import helpers.IntegrationTestConstants.{IndividualURI, basGatewaySignIn}
 import helpers.servicemocks.AuthStub
-import models.status.MandationStatus.Voluntary
-import models.status.MandationStatusModel
-import models.{EligibilityStatus, Yes, YesNo}
 import play.api.http.Status.{OK, SEE_OTHER}
-import play.api.libs.json.Json
-import utilities.SubscriptionDataKeys.SelectedTaxYear
 
 class WhatYouNeedToDoControllerISpec extends ComponentSpecBase {
 
   val serviceNameGovUk = " - Sign up for Making Tax Digital for Income Tax - GOV.UK"
 
   s"GET ${routes.WhatYouNeedToDoController.show.url}" must {
+    "return SEE_OTHER to the login page" when {
+      "the user is unauthenticated" in {
+        AuthStub.stubUnauthorised()
+
+        val result = IncomeTaxSubscriptionFrontend.whatYouNeedToDo()
+
+        result must have(
+          httpStatus(SEE_OTHER),
+          redirectURI(basGatewaySignIn())
+        )
+      }
+    }
+    "return SEE_OTHER to the home page" when {
+      "the user does not have a journey state" in {
+        AuthStub.stubAuthSuccess()
+
+        val result = IncomeTaxSubscriptionFrontend.get("/what-you-need-to-do", includeState = false)
+
+        result must have(
+          httpStatus(SEE_OTHER),
+          redirectURI(controllers.individual.matching.routes.HomeController.index.url)
+        )
+      }
+    }
     "return OK with the page content" in {
       Given("I am authenticated")
       AuthStub.stubAuthSuccess()
-      SessionDataConnectorStub.stubGetAllSessionData(Map(
-        ITSASessionKeys.MANDATION_STATUS -> Json.toJson(MandationStatusModel(Voluntary, Voluntary)),
-        ITSASessionKeys.ELIGIBILITY_STATUS -> Json.toJson(EligibilityStatus(eligibleCurrentYear = true, eligibleNextYear = true, exemptionReason= None))
-      ))
-      IncomeTaxSubscriptionConnectorStub.stubGetSubscriptionDetails(SelectedTaxYear, OK, Json.toJson(testAccountingYearCurrent))
 
       When(s"GET ${routes.WhatYouNeedToDoController.show.url} is called")
       val result = IncomeTaxSubscriptionFrontend.whatYouNeedToDo()
@@ -54,19 +65,39 @@ class WhatYouNeedToDoControllerISpec extends ComponentSpecBase {
     }
   }
 
-  s"POST ${routes.WhatYouNeedToDoController.submit.url}" when {
-    "return a SEE_OTHER to the your income sources page" in {
+  s"POST ${routes.WhatYouNeedToDoController.submit.url}" must {
+    "return SEE_OTHER to the login page" when {
+      "the user is unauthenticated" in {
+        AuthStub.stubUnauthorised()
+
+        val result = IncomeTaxSubscriptionFrontend.submitWhatYouNeedToDo()
+
+        result must have(
+          httpStatus(SEE_OTHER),
+          redirectURI(basGatewaySignIn())
+        )
+      }
+    }
+    "return SEE_OTHER to the home page" when {
+      "the user does not have a journey state" in {
+        AuthStub.stubAuthSuccess()
+
+        val result = IncomeTaxSubscriptionFrontend.post("/what-you-need-to-do", includeJourneyState = false)(Map.empty)
+
+        result must have(
+          httpStatus(SEE_OTHER),
+          redirectURI(controllers.individual.matching.routes.HomeController.index.url)
+        )
+      }
+    }
+    "return a SEE_OTHER to the using software page" in {
       Given("I am authenticated")
       AuthStub.stubAuthSuccess()
-      SessionDataConnectorStub.stubGetAllSessionData(Map(
-        ITSASessionKeys.MANDATION_STATUS -> Json.toJson(MandationStatusModel(Voluntary, Voluntary)),
-        ITSASessionKeys.ELIGIBILITY_STATUS -> Json.toJson(EligibilityStatus(eligibleCurrentYear = true, eligibleNextYear = true, exemptionReason= None))
-      ))
-      IncomeTaxSubscriptionConnectorStub.stubGetSubscriptionDetails(SelectedTaxYear, OK, Json.toJson(testAccountingYearCurrent))
+
       When(s"POST ${routes.WhatYouNeedToDoController.submit.url} is called")
       val result = IncomeTaxSubscriptionFrontend.submitWhatYouNeedToDo()
 
-      Then("The result should be SEE_OTHER redirecting to the accounting period page")
+      Then("The result should be SEE_OTHER redirecting to the using software page")
       result must have(
         httpStatus(SEE_OTHER),
         redirectURI(IndividualURI.usingSoftwareURI)
